01273db591d9062b2fec5af9b2bcd63f32adedbd
Gopi vishwakarma <g2468vi@gmail.com>
2023-06-28 12:26:23 +0530

    code added

--- twitter/src/Pages/Profile.js
+++ twitter/src/Pages/Profile.js
@@ -133,8 +133,103 @@ const Profile = () => {
 
   return (
     <Flex w='100%'>
-      {/* this is left part  */}
-      <Box w={{ base: '100%', sm: '100%', md: '100%', lg: '60%' }} px="20px" overflowY={'auto'} h="100vh" position={'relative'} pt='10px'
+    {/* this is left part  */}
+    <Box w={{ base: '100%', sm: '100%', md: '100%', lg: '65%' }} px="20px"
+      overflowY={'auto'} h="100vh" position={'relative'} pt='10px'
+      css={{
+        '&::-webkit-scrollbar': {
+          width: '4px',
+        },
+        '&::-webkit-scrollbar-track': {
+          width: '6px',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          borderRadius: '24px',
+        },
+      }}
+    >
+      <Box w="100%" h="60px" bg="white" display={{ base: 'none', sm: 'none', md: 'block' }} >
+        <Flex gap='50px'>
+        <Box onClick={() => window.history.back()} p="10px" _hover={{ backgroundColor: 'gray.200' }} borderRadius={'50%'}
+          cursor={'pointer'} fontSize={'20px'}><FaLongArrowAltLeft />
+        </Box>
+        <Flex direction={'column'}>
+          <Text fontSize={'16px'} fontWeight={'bolder'} color="gray.700">{profile.name}</Text>
+          <Text fontSize={'13px'} fontWeight={'bold'} color="gray.500">{post.length} Tweets</Text>
+        </Flex>
+        </Flex>
+      </Box>
+      <Box onClick={() => window.history.back()} p='10px' pb='20px' cursor={'pointer'}
+        fontSize={'20px'} display={{ base: 'block', sm: 'block', md: 'none' }}><FaLongArrowAltLeft /></Box>
+
+      {/* ===================setting up background and user profile pare from here to */}
+      {
+        !profile?.backgroundpic ? <Box h="250px" w='100%' bg="gray.300" pt='60px' position={'relative'}>
+          {
+            profile?._id === token._id ? <Box p='15px' _hover={{ backgroundColor: 'red.100' }}
+              borderRadius={'50%'} position={'absolute'} bottom={0} right={0} cursor={'pointer'}>
+              <Input type='file' id="image-media" accept='image/*' display={'none'} onChange={(e) => setBackground(e.target.files[0])} />
+              <label for="image-media"><BsFillCameraFill fontSize={'35px'} cursor={'pointer'} /></label>
+            </Box> : ""
+          }
+          <Avatar src={profile?.pic} h='120px' w='120px' position={'absolute'} bottom={'-60px'} left="30px" />
+        </Box> : <Box h={{ base: '150px', sm: '200px', md: '250px' }} w='100%' pt='60px' position={'relative'}
+          bgImage={profile.backgroundpic} bgSize={'cover'} bgRepeat={'no-repeat'}>
+          {
+            profile?._id === token._id ? <Box p='15px' bg='red.100' borderRadius={'50%'} position={'absolute'}
+              bottom={0} right={0} cursor={'pointer'}>
+              <Input type='file' id="image-media" accept='image/*' display={'none'}
+                onChange={(e) => setBackground(e.target.files[0])} />
+              <label for="image-media"><BsFillCameraFill fontSize={'35px'} cursor={'pointer'} /></label>
+            </Box> : ""
+          }
+          <Avatar src={profile?.pic} h='120px' w='120px' position={'absolute'} bottom={'-60px'} left="30px" />
+        </Box>
+      }
+      {/* ===============================till here  */}
+
+
+      {/* f==========ollow and unfollow button from here to  */}
+      <Flex justifyContent={'flex-end'} pt="15px">
+        {
+          profile?._id === token._id ? <Button borderRadius={'50px'} border={'1px solid black'}
+            px="25px" py='10px'>Edit Profile</Button> :
+            profile.followers?.includes(token._id) ?
+              <Button borderRadius={'50px'} border={'1px solid black'} px="25px" py='10px'
+                color={followText === 'unfollow' ? 'red' : 'black'} onMouseOver={changeText}
+                onMouseOut={previousText} onClick={unfollowUser}>{followText}</Button> :
+              <Button borderRadius={'50px'} border={'1px solid black'} px="25px" py='10px'
+                bg='black' colorScheme='white' onClick={followUser}>Follow</Button>
+        }
+      </Flex>
+      {/* =============till here  */}
+
+      {/* ============followers and unfollowers  */}
+      <Flex direction={'column'} pt="30px" gap="10px">
+        <Heading fontSize={'18px'}>{profile?.name}</Heading>
+        <Text>joinded june 2023</Text>
+        <Flex gap="30px">
+          <Button variant={'unstyled'}>{follower} Followers</Button>
+          <Button variant={'unstyled'}>{following} Following</Button>
+        </Flex>
+      </Flex>
+
+      {/* =====showing data of logged user from here to  */}
+      <VStack pt="30px">
+        {
+          post?.map(el => (
+            <Post el={el} key={el._id} />
+          ))
+        }
+      </VStack>
+      {/* ==========till here  */}
+    </Box>
+
+    {/* this is right part  */}
+    <Box w='35%' display={{ base: 'none', sm: 'none', md: 'none', lg: 'block' }}>
+      <Flex w='100%' position={'relative'} zIndex={2} direction={'column'}
+        ml='15px' gap='20px' h='100vh' overflowY={'scroll'} pt='10px'
+        fontFamily={'regulare.400'}
         css={{
           '&::-webkit-scrollbar': {
             width: '4px',
@@ -147,133 +242,61 @@ const Profile = () => {
           },
         }}
       >
-        <Box w="100%" h="60px" bg="white" display={{ base: 'none', sm: 'none', md: 'block' }} >
-          <Heading fontSize={'22px'}>Profile</Heading>
-        </Box>
-        <Box onClick={() => window.history.back()} p='10px' pb='20px' cursor={'pointer'} fontSize={'20px'} display={{ base: 'block', sm: 'block', md: 'none' }}><FaLongArrowAltLeft /></Box>
+        {/* search part here  */}
+        <InputGroup w="100%"  >
+          <InputLeftElement pointerEvents='none'>
+            <BiSearch color='gray.500' />
+          </InputLeftElement>
+          <Input type='text' placeholder='search' borderRadius={'50px'}
+            fontSize={'18px'} variant={'filled'} bg='gray.300' />
+        </InputGroup>
 
-        {/* ===================setting up background and user profile pare from here to */}
-        {
-          !profile?.backgroundpic ? <Box h="250px" w='100%' bg="gray.300" pt='60px' position={'relative'}>
-            {
-              profile?._id === token._id ? <Box p='15px' _hover={{ backgroundColor: 'red.100' }} borderRadius={'50%'} position={'absolute'} bottom={0} right={0} cursor={'pointer'}>
-                <Input type='file' id="image-media" accept='image/*' display={'none'} onChange={(e) => setBackground(e.target.files[0])} />
-                <label for="image-media"><BsFillCameraFill fontSize={'35px'} cursor={'pointer'} /></label>
-              </Box> : ""
-            }
-            <Avatar src={profile?.pic} h='120px' w='120px' position={'absolute'} bottom={'-60px'} left="30px" />
-          </Box> : <Box h={{ base: '150px', sm: '200px', md: '250px' }} w='100%' pt='60px' position={'relative'} bgImage={profile.backgroundpic} bgSize={'cover'} bgRepeat={'no-repeat'}>
-            {
-              profile?._id === token._id ? <Box p='15px' bg='red.100' borderRadius={'50%'} position={'absolute'} bottom={0} right={0} cursor={'pointer'}>
-                <Input type='file' id="image-media" accept='image/*' display={'none'} onChange={(e) => setBackground(e.target.files[0])} />
-                <label for="image-media"><BsFillCameraFill fontSize={'35px'} cursor={'pointer'} /></label>
-              </Box> : ""
-            }
-            <Avatar src={profile?.pic} h='120px' w='120px' position={'absolute'} bottom={'-60px'} left="30px" />
-          </Box>
-        }
-        {/* ===============================till here  */}
-
-
-        {/* f==========ollow and unfollow button from here to  */}
-        <Flex justifyContent={'flex-end'} pt="15px">
+        {/* who to follow section here  */}
+        <Flex direction={'column'} p='15px' gap='20px' bg='gray.100'>
+          <Heading fontSize={'22px'}>Who to follow</Heading>
           {
-            profile?._id === token._id ? <Button borderRadius={'50px'} border={'1px solid black'} px="25px" py='10px'>Edit Profile</Button> :
-              profile.followers?.includes(token._id) ?
-                <Button borderRadius={'50px'} border={'1px solid black'} px="25px" py='10px' color={followText === 'unfollow' ? 'red' : 'black'} onMouseOver={changeText} onMouseOut={previousText} onClick={unfollowUser}>{followText}</Button> :
-                <Button borderRadius={'50px'} border={'1px solid black'} px="25px" py='10px' bg='black' colorScheme='white' onClick={followUser}>Follow</Button>
-          }
-        </Flex>
-        {/* =============till here  */}
-
-        {/* ============followers and unfollowers  */}
-        <Flex direction={'column'} pt="30px" gap="10px">
-          <Heading fontSize={'18px'}>{profile?.name}</Heading>
-          <Text>joinded june 2023</Text>
-          <Flex gap="30px">
-            <Button variant={'unstyled'}>{follower} Followers</Button>
-            <Button variant={'unstyled'}>{following} Following</Button>
-          </Flex>
-        </Flex>
-
-        {/* =====showing data of logged user from here to  */}
-        <VStack pt="30px">
-          {
-            post?.map(el => (
-              <Post el={el} key={el._id} />
+            followArr.map((el, ind) => (
+              <Flex justifyContent={'space-between'} key={ind}>
+                <Flex gap='10px'>
+                  <Avatar />
+                  <Flex direction={'column'}>
+                    <Heading fontSize="16px">gopi hii</Heading>
+                    <Text fontSize={'14px'}>@gopi hii</Text>
+                  </Flex>
+                </Flex>
+                <Button bg="black" colorScheme='white' borderRadius={'50px'}>Follow</Button>
+              </Flex>
             ))
           }
-        </VStack>
-        {/* ==========till here  */}
-      </Box>
-      
-      {/* this is right part  */}
-      <Box w='40%' display={{ base: 'none', sm: 'none', md: 'none', lg: 'block' }}>
-        <Flex w='100%' position={'relative'} zIndex={2} direction={'column'} ml='15px' gap='20px' h='100vh' overflowY={'scroll'} pt='10px'
-          fontFamily={'regulare.400'}
-          css={{
-            '&::-webkit-scrollbar': {
-              width: '4px',
-            },
-            '&::-webkit-scrollbar-track': {
-              width: '6px',
-            },
-            '&::-webkit-scrollbar-thumb': {
-              borderRadius: '24px',
-            },
-          }}
-        >
-          {/* search part here  */}
-          <InputGroup w="100%"  >
-            <InputLeftElement pointerEvents='none'>
-              <BiSearch color='gray.500' />
-            </InputLeftElement>
-            <Input type='text' placeholder='search' borderRadius={'50px'} fontSize={'18px'} variant={'filled'} bg='gray.300' />
-          </InputGroup>
 
-          {/* who to follow section here  */}
-          <Flex direction={'column'} p='15px' gap='20px' bg='gray.100'>
-            <Heading fontSize={'22px'}>Who to follow</Heading>
+        </Flex>
+
+        {/* what is happenning secttion her  */}
+        <Flex direction={'column'} justifyContent={'flex-start'} bg='gray.100'>
+          <Heading fontSize='22px' color='#0F1419 ' fontFamily={'revert'}
+            p='15px'>What is happening</Heading>
+          <Flex direction={'column'} >
             {
-              followArr.map((el, ind) => (
-                <Flex justifyContent={'space-between'} key={ind}>
-                  <Flex gap='10px'>
-                    <Avatar />
-                    <Flex direction={'column'}>
-                      <Heading fontSize="16px">gopi hii</Heading>
-                      <Text fontSize={'14px'}>@gopi hii</Text>
-                    </Flex>
+              trendingArr.map((el, ind) => (
+                <Flex direction={'column'} _hover={{ backgroundColor: 'gray.200' }}
+                  px='15px' py='10px' w='100%' key={ind}  >
+                  <Flex justifyContent={'space-between'} w='100%' >
+                    <Text fontSize={'13px'} fontWeight={'bold'} color="gray.600">This is text</Text>
+                    <Heading fontSize='20px' cursor={'pointer'}>...</Heading>
                   </Flex>
-                  <Button bg="black" colorScheme='white' borderRadius={'50px'}>Follow</Button>
+                  <Heading fontSize={'15px'}>#Gopi vishwakarma</Heading>
+                  <Text fontSize={'13px'} fontWeight={'bold'} color="gray.600">25.5k tweets</Text>
                 </Flex>
               ))
             }
-
+            <Heading fontSize={'18px'} color='blue.400' px='15px' py='10px' w='100%'
+              _hover={{ backgroundColor: 'gray.200' }} cursor={'pointer'} >Show more</Heading>
           </Flex>
-
-          {/* what is happenning secttion her  */}
-          <Flex direction={'column'} justifyContent={'flex-start'} bg='gray.100'>
-            <Heading fontSize='22px' color='#0F1419 ' fontFamily={'revert'} p='15px'>What is happening</Heading>
-            <Flex direction={'column'} >
-              {
-                trendingArr.map((el, ind) => (
-                  <Flex direction={'column'} _hover={{ backgroundColor: 'gray.200' }} px='15px' py='10px' w='100%' key={ind}  >
-                    <Flex justifyContent={'space-between'} w='100%' >
-                      <Text color="gray.600" fontSize={'13px'}>This is text</Text>
-                      <Heading fontSize='20px' cursor={'pointer'}>...</Heading>
-                    </Flex>
-                    <Heading fontSize={'15px'}>#Gopi vishwakarma</Heading>
-                    <Text color="gray.600" fontSize={'13px'}>25.5k tweets</Text>
-                  </Flex>
-                ))
-              }
-              <Heading fontSize={'18px'} color='blue.400' px='15px' py='10px' w='100%' _hover={{ backgroundColor: 'gray.200' }} cursor={'pointer'} >Show more</Heading>
-            </Flex>
-          </Flex>
-
         </Flex>
-      </Box>
-    </Flex>
+
+      </Flex>
+    </Box>
+  </Flex>
   )
 }
 

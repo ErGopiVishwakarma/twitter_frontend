5580abeabe6bde0d3eda25194fdb107896f97281
Gopi vishwakarma <g2468vi@gmail.com>
2023-06-28 12:22:52 +0530

    code added

--- twitter/src/Component/homeComponent/Post.js
+++ twitter/src/Component/homeComponent/Post.js
@@ -20,64 +20,80 @@ import DeleteModal from '../DeleteModal'
 
 
 const Post = ({ el }) => {
+    
     const token = JSON.parse(localStorage.getItem('twitteruser'))
-    return (
-        <Flex gap="20px" w="100%">
-            <Box>
-                <NavLink to={`profile/${el?.postedBy?._id}`}>
-                    <Avatar src={el.postedBy.pic} h={{ base: '30px', sm: '40px', md: "40px" }} w={{ base: '30px', sm: '40px', md: "40px" }} />
-                </NavLink>
-            </Box>
-            <Flex w='100%' direction={'column'} alignItems={'flex-start'} gap="8px">
-                <Flex justifyContent={'space-between'} w="100%" >
-                    <NavLink to={`profile/${el?.postedBy?._id}`}>
-                        <Heading fontSize="18px">{el?.postedBy.name}</Heading>
-                    </NavLink>
-                    {/* delete functionality here  */}
-                    <Menu>
-                        <MenuButton>
-                            <Heading fontSize={'20px'}>...</Heading>
-                        </MenuButton>
-                        <MenuList>
-                            <MenuItem color='red' display={el?.postedBy?._id === token.user?._id ? "block" : 'none'}>
-                                <DeleteModal postId={el?._id} />
-                            </MenuItem>
-                            <MenuItem>
-                                <Flex gap='10px' alignItems={'center'}>
-                                    <BiUserPlus fontSize={'22px'} />
-                                    <Text>Follow</Text>
-                                </Flex>
-                            </MenuItem>
-                            <MenuItem >
-                                <Flex gap='10px' alignItems={'center'}>
-                                    <BsCodeSlash fontSize={'22px'} />
-                                    <Text>Emmbed</Text>
-                                </Flex>
-                            </MenuItem>
-                            <MenuItem>
-                                <Flex gap='10px' alignItems={'center'}>
-                                    <RiFlag2Line fontSize={'22px'} />
-                                    <Text>Report</Text>
-                                </Flex>
-                            </MenuItem>
-                        </MenuList>
-                    </Menu>
-                    {/* till here  */}
+    const date = new Date()
+    const postDate = new Date(`${el.createdAt}`)
+   let second = Math.floor(((date - postDate)/1000)/3600)
+   let b = postDate.getDate() 
+  let a = postDate.toLocaleString('default', { month: 'long' }).substring(0,3)
 
-                </Flex>
-                <NavLink to={`post/${el?._id}`}>
-                    
-                        <Text w='100%' display={'flex'} wordBreak={'break-all'}>{el?.content}</Text>
-                  
+    return (
+        <Flex gap="20px" w="100%" px='10px' py='15px' _hover={{backgroundColor:'gray.100'}} boxShadow={'rgba(0, 0, 0, 0.05) 0px 0px 0px 1px'}>
+        <Box>
+            <NavLink to={`profile/${el?.postedBy?._id}`}>
+                <Avatar src={el.postedBy.pic} h={{ base: '30px', sm: '40px', md: "40px" }} w={{ base: '30px', sm: '40px', md: "40px" }} />
+            </NavLink>
+        </Box>
+        <Flex w='100%' direction={'column'} alignItems={'flex-start'} gap="8px">
+            <Flex justifyContent={'space-between'} w="100%" >
+                <NavLink to={`profile/${el?.postedBy?._id}`}>
+                    <Flex alignItems={'center'} gap='3px'>
+                        <Text fontSize={'15px'} fontWeight={'bold'} color="gray.700">{el?.postedBy.name}</Text>
+                        <Text fontSize={'14px'} fontWeight={'bold'} color="gray.500">@{el?.postedBy.name.toLowerCase().replaceAll(" ","")}.</Text>
+                        {
+                            second < 24 ?<Text pl='15px' fontSize={'14px'} fontWeight={'bold'} color="gray.600">{second}h ago</Text>:
+                            <Text pl='15px' fontSize={'14px'} fontWeight={'bold'} color="gray.600">{`${a}${b}`}</Text>
+                        }
+                        
+                        
+                    </Flex>
                 </NavLink>
-                <Box borderRadius={{ base: "15px", md: '25px' }} w="100%">
-                    <Image src={el?.picture} borderRadius={{ base: "15px", md: '25px' }} 
-                     w="100%" 
-                     />
-                </Box>
-                <LCSF postId={el?._id} user={el} />
+                {/* delete functionality here  */}
+                <Menu boxShadow={'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px'}>
+                    <MenuButton>
+                        <Heading fontSize={'20px'}>...</Heading>
+                    </MenuButton>
+                    <MenuList>
+                        <MenuItem color='red' display={el?.postedBy?._id === token._id ? "block" : 'none'}>
+                            <DeleteModal postId={el?._id} />
+                        </MenuItem>
+                        <MenuItem>
+                            <Flex gap='10px' alignItems={'center'}>
+                                <BiUserPlus fontSize={'22px'} />
+                                <Text>Follow</Text>
+                            </Flex>
+                        </MenuItem>
+                        <MenuItem >
+                            <Flex gap='10px' alignItems={'center'}>
+                                <BsCodeSlash fontSize={'22px'} />
+                                <Text>Emmbed</Text>
+                            </Flex>
+                        </MenuItem>
+                        <MenuItem>
+                            <Flex gap='10px' alignItems={'center'}>
+                                <RiFlag2Line fontSize={'22px'} />
+                                <Text>Report</Text>
+                            </Flex>
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
+                {/* till here  */}
+
             </Flex>
+            <NavLink to={`/post/${el?._id}`}>
+
+                <Text w='100%' display={'flex'} wordBreak={'break-all'}>{el?.content}</Text>
+
+            </NavLink>
+            <Box borderRadius={{ base: "15px", md: '17px' }} w="100%">
+                <Image src={el?.picture} borderRadius={{ base: "15px", md: '17px' }}
+                    
+                />
+            </Box>
+            <LCSF postId={el?._id} user={el} />
         </Flex>
+    </Flex>
     )
 }
 
